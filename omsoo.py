3a0004ed380cc2407cb574d20f42983b5b2f50dd
omsoo <onyebuchisundayo@gmail.com>
2018-09-14 12:35:41 +0100

    First github project

---
+++ omsoo.py
@@ -0,0 +1,9 @@
+
+
+print("my name is Onyebuchi Sunday")
+user_mind = input('what do you think about me buddy?')
+if user_mind.lower() == 'perfect':
+    print('thanks buddy')
+else:
+    print('you are not my Nigga')
+
